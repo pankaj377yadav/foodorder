66e29706f785abb1646732324f2d03048e422405
Pankaj Kumar Yadav <pankaj377yadav@gmail.com>
2023-06-09 10:03:55 +0545

    creat login

---
+++ client/src/pages/index.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Login from './login'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <Login/>
+ 
+  )
+}
---
+++ client/src/pages/login/otp/index.js
@@ -0,0 +1,5 @@
+const Login = ()=> {
+    return <h1>otp page</h1>
+}
+
+export default OTP
